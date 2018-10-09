d98a5a5bba00688879a1335ea8d9f0075b776223
nson <thaison1804@gmail.com>
2018-10-09 20:59:52 +0700

    get map, get pos

--- IA.py
+++ IA.py
@@ -15,11 +15,11 @@ def get_maze():
     maze_list = []
     # maze = sys.stdin.readline()
     line = sys.stdin.readline()
-    current_x = 0
+    current_y = 0
     a_x = 0
     a_y = 0
     while line != "\n":
-        current_y = 0
+        current_x = 0
         row_maze_list = []
         for char in line:
             if char != '\n':
@@ -27,18 +27,18 @@ def get_maze():
                 if char == 'A':
                     a_x = current_x
                     a_y = current_y
-            current_y += 1
+            current_x += 1
         maze_list.append(row_maze_list)
         print(row_maze_list, file=sys.stderr)
         line = sys.stdin.readline()
-        current_x += 1
+        current_y += 1
     return maze_list, (a_x, a_y)
 
 
 # solve the maze
 def solve_maze(maze, pos_a):
     print(pos_a, file=sys.stderr)
-    print(maze)
+    print(maze) # print move
 
 
 maze, pos_a = get_maze()
