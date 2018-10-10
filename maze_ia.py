ca6afeb3f766196cdafe3403fcbb1d95f56ffa72
nson <thaison1804@gmail.com>
2018-10-10 13:01:19 +0700

    get all viable node; get all viable neighbor around pos a

--- maze_ia.py
+++ maze_ia.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 import sys
 
+offsets = ((0, 1), (0, -1), (-1, 0), (1, 0))
+
 sys.stdin.readline()
 print("I AM Son\n")
 sys.stdin.readline()
@@ -13,7 +15,7 @@ sys.stdin.readline()
 # get the maze from the VM
 def get_maze():
     maze_list = []
-    # maze = sys.stdin.readline()
+    all_node = []
     line = sys.stdin.readline()
     current_y = 0
     a_x = 0
@@ -27,19 +29,33 @@ def get_maze():
                 if char == 'A':
                     a_x = current_x
                     a_y = current_y
+                if char != 'A' and char != '#':
+                    all_node.append([current_x, current_y])
             current_x += 1
         maze_list.append(row_maze_list)
         print(row_maze_list, file=sys.stderr)
         line = sys.stdin.readline()
         current_y += 1
-    return maze_list, (a_x, a_y)
+    return maze_list, (a_x, a_y), all_node
 
+
+# check neighbors
+def get_neighbors(all_node, pos_a):
+    viable_neighbors = []
+    for offset in offsets:
+        neighbor = [pos_a[0] + offset[0], pos_a[1] + offset[1]]
+        if neighbor in all_node:
+            viable_neighbors.append(neighbor)
+    return viable_neighbors
 
 # solve the maze
 def solve_maze(maze, pos_a):
     print(pos_a, file=sys.stderr)
+    print(all_node, file=sys.stderr)
     print(maze) # print move
 
 
-maze, pos_a = get_maze()
+maze, pos_a, all_node = get_maze()
+neighbors = get_neighbors(all_node, pos_a)
+print(neighbors, file=sys.stderr)
 solve_maze(maze, pos_a)
